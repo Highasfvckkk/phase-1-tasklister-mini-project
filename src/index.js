5d2ead344d3f28127cf27134bd437375347a4985
Kinoti <vickykinoti001@gmail.com>
2025-12-24 14:46:46 +0300

    Second attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const tasks = document.getElementById("tasks");
+  const input = document.getElementById("Task-description");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    tasks.appendChild(li);
+    form.reset();
+  });
 });
